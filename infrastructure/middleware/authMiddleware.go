17d594b921d1d88c3dcc4e7119795f951544e740
Marija Klještan <marijakljestan@uns.ac.rs>
2022-05-20 00:53:53 +0200

    feat: cors settings

--- infrastructure/middleware/authMiddleware.go
+++ infrastructure/middleware/authMiddleware.go
@@ -4,13 +4,34 @@ import (
 	"fmt"
 	"github.com/casbin/casbin/v2"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/spf13/viper"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 )
 
+func allowedOrigin(origin string) bool {
+	if viper.GetString("cors") == "*" {
+		return true
+	}
+	if matched, _ := regexp.MatchString(viper.GetString("cors"), origin); matched {
+		return true
+	}
+	return false
+}
+
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if allowedOrigin(r.Header.Get("Origin")) {
+			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
 
 		fmt.Println(r.URL.Path)
 		if r.URL.Path == "/registration" || r.URL.Path == "/login" {
